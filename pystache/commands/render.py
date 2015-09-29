6b27d9d0c142327f01e5dcfe4695e8b37d2d1c7f
Charbel Jacquin <charbel.jacquin@camptocamp.com>
2015-09-29 14:18:16 +0200

    simplify, using the fact that yaml is a superset of json

--- pystache/commands/render.py
+++ pystache/commands/render.py
@@ -7,22 +7,6 @@ Run this script using the -h option for command-line help.
 
 """
 import sys
-try:
-    import json
-except ImportError:
-    # The json module is new in Python 2.6, whereas simplejson is
-    # compatible with earlier versions.
-    try:
-        import simplejson as json
-    except ImportError:
-        # Raise an error with a type different from ImportError
-        # as a hack around this issue:
-        #   http://bugs.python.org/issue7559
-        from sys import exc_info
-        ex_type, ex_value, tb = exc_info()
-        new_ex = Exception("%s: %s" % (ex_type.__name__, ex_value))
-        raise new_ex.__class__, new_ex, tb
-
 import yaml
 
 # The optparse module is deprecated in Python 2.7 in favor of argparse.
@@ -38,13 +22,18 @@ from optparse import OptionParser
 from pystache.renderer import Renderer
 
 USAGE = """\
-%prog [-hy] template context
+%prog [-hy] template [context]
 
 Render a mustache template with the given context.
 
 positional arguments:
+
   template    A filename or template string.
   context     A yaml or json filename, or a json string
+
+if context is omitted, pystache read a YAML frontmatter
+as render context from standard input if not a tty.
+
 """
 
 def parse_args(sys_argv, usage):
@@ -58,8 +47,17 @@ def parse_args(sys_argv, usage):
     parser.add_option('-v', '--version', action="store_true",
                       dest="show_version", default=False,
                       help="show version and exit")
-    parser.add_option('-y', action="store_true", dest="yaml", default=False)
+    parser.add_option('-y', action="store_true", dest="yaml", default=False,
+                      help="accept yaml as context format")
     options, args = parser.parse_args(args)
+    if options.show_version:
+        import pystache
+        print("pystache %s" % pystache.__version__)
+        sys.exit(0)
+    if len(args) == 1:
+        template, context = args[0], None
+    else:
+        template, context = args
 
     if options.show_version:
         import pystache
@@ -81,43 +79,20 @@ def parse_args(sys_argv, usage):
 #
 MARKER = '---\n'
 
-
-def read_yaml_frontmatter(iterable):
-    """read mustache's style YAML frontmatter from an iterable
-
-    the iterable is expected to return text lines ending with '\n'
-    """
-    fm_started = False
-    frontmatter = []
-    for line in iterable:
-        if MARKER == line:
-            if fm_started:
-                break
-            fm_started = True
-            continue
-        if fm_started:
-            frontmatter.append(line)
-    frontmatter = ''.join(frontmatter)
-    return yaml.load(frontmatter), iterable
-
-
 def arg2text(arg):
     """ get text from comand line arg """
     import errno
-    if not isinstance(arg, str):
-        return arg.read().decode('utf-8')
-    else:
-        try:
-            if sys.version_info.major == 3:
-                with open(arg, encoding='utf-8') as data:
-                    return data.read()
-            else:
-                with open(arg, 'rb') as data:
-                    return data.read().decode('utf-8')
-        except IOError, err:
-            if err.errno == errno.ENOENT:
-                # not a file, assumming first arg is template literal
-                return arg
+    try:
+        if sys.version_info.major == 3:
+            with open(arg, encoding='utf-8') as data:
+                return data.read()
+        else:
+            with open(arg, 'rb') as data:
+                return data.read().decode('utf-8')
+    except IOError, err:
+        if err.errno == errno.ENOENT:
+            # not a file, assumming first arg is template literal
+            return arg
 
 def main(argv=None):
     if argv is None:
@@ -126,11 +101,9 @@ def main(argv=None):
     template, context, options = parse_args(argv, USAGE)
 
     if context is None and not sys.stdin.isatty():
-        user_context = read_yaml_frontmatter(sys.stdin)
+        user_context = yaml.load(sys.stdin)
     elif context:
-        content = arg2text(context)
-        user_context = (yaml.load(content) if options.yaml else
-                        json.loads(content))
+        user_context = yaml.load(arg2text(context))
     else:
         user_context = {}
 
